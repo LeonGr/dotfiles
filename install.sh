493acfbd57eae2df4eb7a8ad955bbfb269e0b5ff
Leon <Leongrasmeijer@gmail.com>
2016-01-14 14:11:49 +0100

    Added delte stuff

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+rm ~/.config/nvim/init.vim
 ln -s ~/dotfiles/init.vim ~/.config/nvim
+rm ~/.oh-my-zsh/themes/leon.zsh-theme
 ln -s ~/dotfiles/leon.zsh-theme ~/.oh-my-zsh/themes
