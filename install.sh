509f5ce6730dcddd1955c519acb032b9d02641b4
Leon <Leongrasmeijer@gmail.com>
2016-01-14 14:30:22 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+rm -r ~/.config/nvim/autoload
+mkdir ~/.config/nvim/autoload
+cp ~/dotfiles/plug.vim ~/.config/nvim/autoload
 rm ~/.config/nvim/init.vim
 ln -s ~/dotfiles/init.vim ~/.config/nvim
+
 rm ~/.oh-my-zsh/themes/leon.zsh-theme
 ln -s ~/dotfiles/leon.zsh-theme ~/.oh-my-zsh/themes
