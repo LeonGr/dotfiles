759d9be088dc62572341a486b9b5d65867b5a084
Leon <Leongrasmeijer@gmail.com>
2016-01-28 15:54:12 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
-rm -r ~/.config/nvim/autoload
-mkdir ~/.config/nvim/autoload
-cp ~/dotfiles/plug.vim ~/.config/nvim/autoload
-rm -f ~/.config/nvim/init.vim
+rm ~/.config/nvim/init.vim
 ln -s ~/dotfiles/init.vim ~/.config/nvim
 
 rm ~/.oh-my-zsh/themes/leon.zsh-theme
 ln -s ~/dotfiles/leon.zsh-theme ~/.oh-my-zsh/themes
+
+rm ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+rm ~/GitClones/tmux-powerline/themes/mytheme.sh
+ln -s ~/dotfiles/mytheme.sh ~/GitClones/tmux-powerline/themes/mytheme.sh
