5a85c3a728255740b2f62350d51e6d243d10473e
LeonGr <leongrasmeijer@gmail.com>
2017-05-03 13:29:24 +0000

    update

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bard
+# Launch bar
 polybar mybar &
 
 echo "Bars launched..."
