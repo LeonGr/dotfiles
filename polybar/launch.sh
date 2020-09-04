74c1a70f799413b87acf7c4b4e7770d2365de97e
LeonGr <leongrasmeijer@gmail.com>
2020-09-04 16:21:25 +0200

    Update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,10 +4,13 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+#while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-MONITOR=eDP-1 polybar mybar &
+#MONITOR=eDP-1 polybar mybar &
 #MONITOR=DisplayPort-0 polybar mybar &
+echo "---" | tee -a /tmp/polybar.log
+#MONITOR=HDMI2 polybar mybar >>/tmp/polybar.log 2>&1 &
+MONITOR=DP-1 polybar mybar #>>/tmp/polybar.log 2>&1 &
 
 echo "Bars launched..."
