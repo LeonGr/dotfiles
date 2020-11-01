e92a2d79b127edbc2ed7c82cc3e467ac48d33414
LeonGr <leongrasmeijer@gmail.com>
2020-11-01 14:05:21 +0100

    polybar - update launch script, remove fontawesome font, use paru in aur update script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,5 +13,6 @@ echo "---" | tee -a /tmp/polybar.log
 #MONITOR=HDMI2 polybar mybar >>/tmp/polybar.log 2>&1 &
 primaryMonitor=$(xrandr | grep -E " connected primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
 MONITOR=$primaryMonitor polybar mybar >>/tmp/polybar.log 2>&1 &
+#MONITOR=DP-1 polybar mybar >>/tmp/polybar.log 2>&1 &
 
 echo "Bars launched..."
