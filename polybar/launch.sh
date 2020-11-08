1b23dd97265d899ab64b2f12edd4364886548fbf
LeonGr <leongrasmeijer@gmail.com>
2020-11-08 18:13:49 +0100

    polybar - automatic resolution based config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,13 +6,38 @@ killall -q polybar
 # Wait until the processes have been shut down
 #while pgrep -x polybar >/dev/null; do sleep 1; done
 
+RESOLUTION=$(xrandr | grep -E "connected primary" | sed -e "s/.*connected primary//" | sed -e "s/\s(.*//" | sed -e "s/\s//")
+
+if [ "$RESOLUTION" = "3840x2160+0+0" ]; then
+    echo "4K"
+    width="98%"
+    height="64"
+    offset="1%"
+    font0="Inconsolata:bold:size=18;1.5"
+    font1="TerminessTTF Nerd Font Mono:size=30;2"
+    font2="M+ 1mn:bold:pixelsize=14;0; ; for Chinese/Japanese numerals (ttf-mplus)"
+    left="bspwm"
+    center="xwindow"
+    right="updates-pacman-aurhelper sep network sep date sep pulseaudio"
+    interface="enp6s0"
+else
+    echo "1080p"
+    width="96%"
+    height="48"
+    offset="2%"
+    font0="Inconsolata:bold:size=12;1.5"
+    font1="TerminessTTF Nerd Font Mono:size=18;2"
+    font2="M+ 1mn:bold:pixelsize=10;0; ; for Chinese/Japanese numerals (ttf-mplus)"
+    left="bspwm xwindow"
+    center=""
+    right="updates-pacman-aurhelper sep filesystem sep battery sep date sep pulseaudio"
+    interface="wlp2s0"
+fi
+
 # Launch bar
-#MONITOR=eDP-1 polybar mybar &
-#MONITOR=DisplayPort-0 polybar mybar &
 echo "---" | tee -a /tmp/polybar.log
-#MONITOR=HDMI2 polybar mybar >>/tmp/polybar.log 2>&1 &
 primaryMonitor=$(xrandr | grep -E " connected primary" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-MONITOR=$primaryMonitor polybar mybar >>/tmp/polybar.log 2>&1 &
-#MONITOR=DP-1 polybar mybar >>/tmp/polybar.log 2>&1 &
+WIDTH=$width HEIGHT=$height OFFSET=$offset FONT0=$font0 FONT1=$font1 FONT2=$font2 LEFT=$left CENTER=$center RIGHT=$right INTERFACE=$interface MONITOR=$primaryMonitor polybar mybar >>/tmp/polybar.log 2>&1 &
+
 
 echo "Bars launched..."
