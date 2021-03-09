763284e146df192c75c6804fd4076abf599ddb55
LeonGr <leongrasmeijer@gmail.com>
2021-03-09 09:39:36 +0100

    neovim - add lua language server

--- vim/lsp.lua
+++ vim/lsp.lua
@@ -93,3 +93,29 @@ lspconfig.vuels.setup({ on_attach=on_attach })
 -- Enable Clangd (C/C++)
 lspconfig.clangd.setup({ on_attach=on_attach })
 
+-- Enable lua-language-server (Lua)
+lspconfig.sumneko_lua.setup({
+    on_attach=on_attach ;
+    cmd = { "/bin/lua-language-server", "-E", vim.fn.stdpath('cache').."/lspconfig/sumneko_lua/lua-language-server/main.lua" };
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';'),
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                },
+            },
+        },
+    },
+})
