28ad830be91cf50f26f0a14cc3fd57266e786043
LeonGr <mail+github@leongr.nl>
2021-09-05 16:18:47 +0200

    scripts - improve vpn status script

--- polybar/scripts/vpn-status.sh
+++ polybar/scripts/vpn-status.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-curl -s https://am.i.mullvad.net/connected | sd '^.*\(server (.*)\).*$' '$1' | rg -o '[^ ]+$'
+json=$(curl -s https://am.i.mullvad.net/json)
+server=$(echo "$json" | jq --raw-output '.mullvad_exit_ip_hostname')
+
+if [ "$server" = "null" ]; then
+    echo "$json" | jq --raw-output '.ip'
+else
+    echo "$server"
+fi
