d715ae691b5ca519c7c0e3081624d3173911133c
LeonGr <mail+github@leongr.nl>
2022-09-03 12:44:48 +0200

    neovim - add keybind for 'search selection'

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -179,6 +179,26 @@ require'telescope'.setup {
 require'telescope'.load_extension('fzf')
 require'telescope'.load_extension('dap')
 
+local function get_visual_selection()
+    local s_start = vim.fn.getpos("v")
+    local s_end = vim.fn.getpos(".")
+    local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+    local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+    lines[1] = string.sub(lines[1], s_start[3], -1)
+    if n_lines == 1 then
+        lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+    else
+        lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+    end
+    return table.concat(lines, '\n')
+end
+
+-- search for the current selection
+vim.keymap.set('v', '<Leader>R', function()
+    local text = get_visual_selection()
+    require('telescope.builtin').grep_string({ search = text })
+end, { expr = false })
+
 ---- glepnir/galaxyline.nvim
 
 -- config adapted from https://github.com/siduck76/NvChad/blob/main/lua/plugins/statusline.lua
