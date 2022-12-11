dc6335c02776be7eddc2f6bc786c16271d4bf645
LeonGr <mail+github@leongr.nl>
2022-12-11 12:17:04 +0100

    neovim - move rust-analyzer options to rust-tools opts

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -21,25 +21,53 @@ client_capabilities.textDocument.completion.completionItem.resolveSupport = {
 local capabilities = require('cmp_nvim_lsp').default_capabilities(client_capabilities)
 
 -- Enable rust_analyzer (Rust)
-lspconfig.rust_analyzer.setup({
-    capabilities=capabilities,
+lspconfig.rust_analyzer.setup({ capabilities=capabilities })
+
+-- Configure LSP through rust-tools.nvim plugin.
+-- rust-tools will configure and enable certain LSP features for us.
+-- See https://github.com/simrat39/rust-tools.nvim#configuration
+-- From https://sharksforarms.dev/posts/neovim-rust/
+local opts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = true,
+      -- parameter_hints_prefix = "",
+      -- other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
     settings = {
-        ["rust-analyzer"] = {
-            checkOnSave = {
-                command = "clippy"
-            },
-            procMacro = {
-                enable = true,
-            },
-            cargo = {
-                allFeatures = true;
-            }
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+          extraArgs = {"--", "-W", "clippy::pedantic"},
+        },
+        procMacro = {
+            enable = true,
+        },
+        cargo = {
+            allFeatures = true;
         }
-    }
-})
+      },
+    },
+  },
+}
 
 -- Enable rust-tools
-require('rust-tools').setup({})
+require('rust-tools').setup(opts)
 
 -- Enable hls (Haskell)
 lspconfig.hls.setup({ capabilities=capabilities; on_attach=on_attach })
