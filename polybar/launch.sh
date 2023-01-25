0105a7fa876b0c716afbc59261a8b5b154dda6a4
LeonGr <mail+github@leongr.nl>
2023-01-25 10:39:33 +0100

    i3 - switch from bspwm to i3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,13 +11,16 @@ RESOLUTION=$(xrandr | rg "connected primary" | sd '.*connected primary (.*?) .*'
 if [ "$RESOLUTION" = "3840x2160+3840+0" ]; then
     echo "4K"
     width="100%"
-    height="65"
-    underline_size="5"
+    # height="65"
+    height="40"
+    # underline_size="5"
+    underline_size="3"
     offset="0%"
-    font0="DejaVu Sans Mono:bold:size=18;1.5"
-    font1="TerminessTTF Nerd Font Mono:size=30;2"
+    font0="DejaVu Sans Mono:bold:size=14;1.5"
+    font1="TerminessTTF Nerd Font Mono:size=24;2"
     font2="M+ 1mn:bold:pixelsize=14;0; ; for Chinese/Japanese numerals (ttf-mplus)"
-    left="bspwm"
+    # left="bspwm"
+    left="i3"
     center="xwindow"
     right="updates-pacman-aurhelper sep vpn-status sep network sep date sep pulseaudio powermenu"
     interface="enp7s0"
@@ -30,7 +33,8 @@ else
     font0="DejaVu Sans Mono:bold:size=12;1.5"
     font1="TerminessTTF Nerd Font Mono:size=18;2"
     font2="M+ 1mn:bold:pixelsize=10;0; ; for Chinese/Japanese numerals (ttf-mplus)"
-    left="bspwm xwindow"
+    # left="bspwm xwindow"
+    left="i3 xwindow"
     center=" "
     right="updates-pacman-aurhelper sep filesystem sep battery sep date sep pulseaudio"
     interface="wlp2s0"
@@ -41,5 +45,20 @@ primaryMonitor=$(xrandr | rg "connected primary" | sd '^(.*?) .*' '$1')
 echo "primary: $primaryMonitor"
 echo "---" | tee -a /tmp/polybar.log
 WIDTH=$width HEIGHT=$height UNDERLINE_SIZE=$underline_size OFFSET=$offset FONT0=$font0 FONT1=$font1 FONT2=$font2 LEFT=$left CENTER=$center RIGHT=$right INTERFACE=$interface MONITOR=$primaryMonitor polybar mybar >>/tmp/polybar.log 2>&1 &
+
+# Launch simplified bar on non-primary monitors
+otherMonitors=$(xrandr | rg " connected " | rg -v "primary" | sd '^(.*?) .*' '$1')
+for monitor in $otherMonitors
+do
+    echo $monitor
+    center=""
+    right=""
+    font0="DejaVu Sans Mono:bold:size=14;1.5"
+    font1="TerminessTTF Nerd Font Mono:size=18;2"
+    font2="M+ 1mn:bold:pixelsize=10;0; ; for Chinese/Japanese numerals (ttf-mplus)"
+    height="30"
+    underline_size="2"
+    WIDTH=$width HEIGHT=$height UNDERLINE_SIZE=$underline_size OFFSET=$offset FONT0=$font0 FONT1=$font1 FONT2=$font2 LEFT=$left CENTER=$center RIGHT=$right INTERFACE=$interface MONITOR=$monitor polybar sidebar >>/tmp/polybar.log 2>&1 &
+done
 
 echo "Bars launched..."
