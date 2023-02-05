63deac75668d9249ae9903e21d5f7587ae52f389
LeonGr <mail+github@leongr.nl>
2023-02-05 13:59:54 +0100

    neovim - disable tmux.nvim copy_sync

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -239,14 +239,7 @@ require'tmux'.setup {
     copy_sync = {
         -- enables copy sync and overwrites all register actions to
         -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
-        enable = true,
-
-        -- TMUX >= 3.2: yanks (and deletes) will get redirected to system
-        -- clipboard by tmux
-        redirect_to_clipboard = false,
-
-        -- synchronizes registers *, +, unnamed, and 0 till 9 with tmux buffers.
-        sync_registers = true,
+        enable = false,
     },
     navigation = {
         -- enables default keybindings (C-hjkl) for normal mode
