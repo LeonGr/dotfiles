1b9127a4572fa06c6e7992680e4aed8efb080196
LeonGr <mail+github@leongr.nl>
2023-05-23 17:01:52 +0200

    polybar - remove separator from laptop polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -36,7 +36,7 @@ else
     # left="bspwm xwindow"
     left="i3 xwindow"
     center=" "
-    right="updates-pacman-aurhelper sep filesystem sep battery sep date sep pulseaudio powermenu"
+    right="updates-pacman-aurhelper filesystem battery date pulseaudio powermenu"
     interface="wlp2s0"
 fi
 
