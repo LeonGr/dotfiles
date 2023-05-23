4a389eaabc9b3634f6fb78e18b9cfcfe1600c3cc
LeonGr <mail+github@leongr.nl>
2023-05-23 16:56:30 +0200

    polybar - update updates-pacman-aurhelper.sh script
    - use checkupdates again, it's more stable.

--- polybar/scripts/updates-pacman-aurhelper.sh
+++ polybar/scripts/updates-pacman-aurhelper.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 # based on: https://github.com/polybar/polybar-scripts/tree/master/polybar-scripts/updates-pacman-aurhelper
 
-pacman_updates=$(pacman -Quq 2> /dev/null)
-paru_updates=$(paru -Quq 2> /dev/null)
-
-all_updates="${pacman_updates}\n${paru_updates}"
-
-if [[ $all_updates = "\n" ]]; then
-    updates=0
-else
-    updates=$(echo -e "$all_updates" | sort -u | wc -l)
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
 fi
 
-if [ "$updates" -gt 0 ]; then
-    echo " $updates"
-else
-    echo " $updates"
+if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
+    updates_aur=0
 fi
+
+updates=$((updates_arch + updates_aur))
+
+echo " $updates"
