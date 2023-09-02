5cb6794d50142bb7ce75538f77592319c4ad2a8e
LeonGr <mail+github@leongr.nl>
2023-09-02 08:10:46 +0200

    fish - add 'git diff --staged' alias 'gds'

--- fish/git.fish
+++ fish/git.fish
@@ -14,6 +14,9 @@ alias gup='git pull --rebase'
 alias gp='git push'
 #compdef _git gp=git-push
 alias gd='git diff'
+#compdef _git gd=git-diff
+alias gds='git diff --staged'
+#compdef _git gd=git-diff
 
 function gdv
   git diff -w $argv | view -
