2cc544b11b0c0e9a17676e2f67fbccab557579a1
LeonGr <mail+github@leongr.nl>
2024-09-16 14:09:15 +0200

    neovim - remove deprecated gitsigns config

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -240,11 +240,11 @@ require("mason-lspconfig").setup({
 ---- lewis6991/gitsigns.nvim
 require('gitsigns').setup {
     signs = {
-        add          = {hl = 'GitSignsAdd'   , text = '┃', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-        change       = {hl = 'GitSignsChange', text = '┇', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-        delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-        topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-        changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        add          = { text = '┃' },
+        change       = { text = '┇' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
     },
 }
 
