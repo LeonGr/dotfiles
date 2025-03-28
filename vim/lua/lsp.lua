990518fcd22d0876af4278fa783345aeea9da939
LeonGr <mail+github@leongr.nl>
2025-03-28 09:14:09 +0100

    neovim - use new way of configuring diagnostics

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -1,12 +1,10 @@
 -- configure diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        underline = true,
-        virtual_text = true,
-        signs = true,
-        update_in_insert = true,
-    }
-)
+vim.diagnostic.config({
+    underline = true,
+    virtual_text = true,
+    signs = true,
+    update_in_insert = true,
+})
 
 -- set inline diagnostics prefix
 vim.diagnostic.config({
